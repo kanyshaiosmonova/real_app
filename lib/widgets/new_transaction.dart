2c7ddc58d10f0107311d401715aec0df70fa00c4
Kanyshai Osmonova <kanyshai.osmonova.kk@gmail.com>
2023-02-15 21:42:07 +0600

    initial commit

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+            ),
+            ElevatedButton(
+              child: Text('Add Transaction'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor:
+                    Colors.white, // change background color of button
+                backgroundColor: Colors.purple, // change text color of button
+              ),
+              onPressed: () {
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
